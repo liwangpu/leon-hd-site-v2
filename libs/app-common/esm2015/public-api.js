31d83cb8c1c1e3c089217978abe5610064f8ff46
Leon Pu <puliwang@cxist.com>
2019-10-18 18:23:38 +0800

    把basic,common发布到registry

--- libs/app-common/esm2015/public-api.js
+++
@@ -1,6 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-export { APISERVER } from './app-tokens';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL2FwcC1jb21tb24vIiwic291cmNlcyI6WyJwdWJsaWMtYXBpLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSwwQkFBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL2FwcC10b2tlbnMnOyJdfQ==
